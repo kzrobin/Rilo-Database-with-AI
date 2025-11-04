6886fefda9c0905c8b94ed585255ad4c9cda76e8
Kz Robin <khalekuzzamanrobin@gmail.com>
2025-11-04 11:27:27 +0600

    Setting up Project

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+
+const connectToDB = require("./db/db");
+const cookieParser = require("cookie-parser");
+connectToDB();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEMD_URL.split(","),
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+module.exports = app;
