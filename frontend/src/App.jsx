b37d317cacdbee97b270c9e702cfe13db7c0da27
Md.Ripon Mia <alakandaripon@gmail.com>
2025-11-05 09:23:30 +0600

    Edited

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          Ruman and Ripon {count}
+          only ripon {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
