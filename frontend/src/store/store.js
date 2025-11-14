d82acc93e9d0cee8f8b0208e3097c49285b0ebdf
Kz Robin <khalekuzzamanrobin@gmail.com>
2025-11-14 10:59:49 +0600

    show product is done

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
-import adminProductsSlice from "./admin/products-slice";
+import adminProductsReducer from "./admin/products-slice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    adminProducts: adminProductsSlice,
+    adminProducts: adminProductsReducer,
   },
 });
 
